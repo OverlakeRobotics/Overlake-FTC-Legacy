6cc21ed42d740eaf43b3ecccef4ff7db4066bfdb
Jack Shew <jack@shew.net>
2017-11-03 12:39:25 -0700

    These are all of the TeleOp Systems and the Op Mode. **ElevatorSystem does not work too good ya
    ControllerOpMode is the competition op mode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/ElevatorSystem.java
@@ -0,0 +1,223 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.qualcomm.hardware.ams.AMSColorSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+/**
+ * Created by jacks on 10/20/2017.
+ */
+
+public class ElevatorSystem {
+    private ConfigParser config;
+    private DcMotor elevator;
+    private DigitalChannel touchSensorBottom;
+    private DigitalChannel touchSensorTop;
+    int encoderVal;
+    int position;
+    private int loadPos1Ticks = 0;
+    private int loadPos2Ticks = 100;
+    private int unloadPos1Ticks = 400;
+    private int unloadStackTicks = 400;
+    private int unloadPos2Ticks = 600;
+    private int unloadPos3Ticks = 800;
+
+    private boolean debouncing = false;
+    private ElapsedTime debounceTime = new ElapsedTime();
+
+
+    boolean isAtTop = false;
+    boolean isAtBottom = false;
+    private double negativePower = -0.3;
+    private double positivePower = 0.3;
+
+    Telemetry telemetry;
+
+    public ElevatorSystem(HardwareMap map, Telemetry telemetry) {
+        this.telemetry = telemetry;
+        this.elevator = map.dcMotor.get("elevator");
+        this.touchSensorBottom = map.get(DigitalChannel.class, "touchBottom");
+        this.touchSensorTop = map.get(DigitalChannel.class, "touchTop");
+        elevator.setDirection(DcMotor.Direction.REVERSE);
+
+    }
+
+    //for autonomous
+    public void goToZero(Telemetry telemetry){
+
+        //On Rev, when configuring use the second input in digital
+        touchSensorBottom.setMode(DigitalChannel.Mode.INPUT);
+
+        while(touchSensorBottom.getState()==true) {
+            telemetry.addData("touch Sensor" , touchSensorBottom.getState());
+            elevator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            elevator.setPower(-0.5);
+
+        }
+        elevator.setPower(0.0);
+        encoderVal = elevator.getCurrentPosition();
+        position = 0;
+
+    }
+
+    public void runMotorDown() {
+        elevator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        elevator.setPower(negativePower);
+
+    }
+
+    public void runMotorUp() {
+        elevator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        elevator.setPower(positivePower);
+
+    }
+
+    public void checkForBottom(Telemetry telemetry){
+
+        //On Rev, when configuring use the second input in digital
+        touchSensorBottom.setMode(DigitalChannel.Mode.INPUT);
+        if((touchSensorBottom.getState() == false) && !isAtBottom) {
+            elevator.setPower(0.0);
+            encoderVal = elevator.getCurrentPosition();
+            position = loadPos1Ticks;
+            isAtBottom = true;
+        } else {
+            if (!debouncing)
+            {
+                debounceTime.reset();
+            }
+            else
+            {
+                if (debounceTime.milliseconds() > 50) {
+                    isAtBottom = (touchSensorBottom.getState() == false);
+                    debouncing = false;
+                }
+            }
+
+        }
+    }
+
+    public void checkForTop() {
+        touchSensorTop.setMode(DigitalChannel.Mode.INPUT);
+        if((touchSensorTop.getState() == false) && !isAtTop) {
+            elevator.setPower(0.0);
+            isAtTop = true;
+        }         if((touchSensorBottom.getState() == false) && !isAtBottom) {
+            elevator.setPower(0.0);
+            encoderVal = elevator.getCurrentPosition();
+            position = unloadPos3Ticks;
+            isAtTop = true;
+        } else {
+            if (!debouncing)
+            {
+                debounceTime.reset();
+            }
+            else
+            {
+                if (debounceTime.milliseconds() > 50) {
+                    isAtTop = (touchSensorBottom.getState() == false);
+                    debouncing = false;
+                }
+            }
+
+        }
+    }
+
+//dpad up
+    public void goToLoadPos2() {
+        elevator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        elevator.setTargetPosition(encoderVal + loadPos2Ticks);
+        telemetry.addData("position: " , position);
+        telemetry.addData("to: ", loadPos2Ticks);
+        if(position > loadPos2Ticks) {
+
+            elevator.setPower(negativePower);
+        } else {
+            elevator.setPower(positivePower);
+
+        }
+        position = loadPos2Ticks;
+    }
+
+    //B button
+    public void goToUnloadPos1() {
+        elevator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        elevator.setTargetPosition(encoderVal + unloadPos1Ticks);
+        if(position > unloadPos1Ticks) {
+            elevator.setPower(negativePower);
+        } else {
+            elevator.setPower(positivePower);
+
+        }
+
+        position = unloadPos1Ticks;
+    }
+
+    //X button
+    public void goToUnloadPos2() {
+        elevator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        elevator.setTargetPosition(encoderVal + unloadPos2Ticks);
+        telemetry.addData("position: " , position);
+        telemetry.addData("to: ", loadPos2Ticks);
+        double power;
+        if(position > unloadPos2Ticks) {
+            power = -0.3;
+        } else {
+            power = 0.3;
+        }
+        telemetry.addData("power: " , power);
+        elevator.setPower(power);
+
+
+        position = unloadPos2Ticks;
+    }
+
+    //y button
+    public void goToUnloadPos3() {
+        elevator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        elevator.setTargetPosition(encoderVal + unloadPos3Ticks);
+        if(position > unloadPos3Ticks) {
+            elevator.setPower(negativePower);
+        } else {
+            elevator.setPower(positivePower);
+
+        }
+
+        position = unloadPos3Ticks;
+    }
+
+    public void goToStackPos() {
+        elevator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        elevator.setTargetPosition(encoderVal + unloadStackTicks);
+        if(position > unloadStackTicks) {
+            elevator.setPower(positivePower);
+        } else {
+            elevator.setPower(negativePower);
+
+        }
+
+        position = unloadStackTicks;
+    }
+
+
+    public enum MotorPositions {
+        LOAD_POSITION1,
+        LOAD_POSITION2,
+        UNLOAD_POSITION1,
+        STACK_POSITION_UNLOAD,
+        UNLOAD_POSITION2,
+        UNLOAD_POSITION3,
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ControllerOpMode.java
@@ -0,0 +1,277 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.robot.*;
+import org.firstinspires.ftc.teamcode.util.Handler;
+
+/**
+ * Created by jacks on 10/5/2017.
+ */
+@TeleOp(name="ContollerOpMode", group="TeleOp")
+public class ControllerOpMode extends OpMode {
+    private ClawSystem claw;
+    private ElevatorSystem elevator;
+    private Button elevatorTopPos;
+    private Button clawRestPosition;
+    private Button clawLoadPosition;
+    private Button clawReleasePosition;
+    private Button elevatorLoadPosition1;
+    private Button elevatorStackPosition;
+    private Button elevatorLoadPosition2;
+    private Button elevatorUnloadPosition1;
+    private Button elevatorUnloadPosition2;
+    private Button elevatorUnloadPosition3;
+    MecanumDriveSystem driveSystem;
+
+
+    public ControllerOpMode(){
+
+    }
+
+    @Override
+    public void init(){
+        claw = new ClawSystem(this.hardwareMap);
+        elevator = new ElevatorSystem(this.hardwareMap, telemetry);
+
+        this.driveSystem = new MecanumDriveSystem();
+        this.driveSystem.init(this.hardwareMap);
+
+        //Claw
+        this.clawLoadPosition = new Button();
+        this.clawLoadPosition.isPressed =
+                new Func<Boolean>()
+                {
+                    @Override
+                    public Boolean value()
+                    {
+                        return gamepad1.right_trigger>0.75;
+                    }
+                };
+        this.clawLoadPosition.pressedHandler =
+                new Handler()
+                {
+                    @Override
+                    public void invoke()
+                    {
+                        claw.setLoadPosition();
+                    }
+                };
+
+        this.clawReleasePosition = new Button();
+        this.clawReleasePosition.isPressed =
+                new Func<Boolean>()
+                {
+                    @Override
+                    public Boolean value()
+                    {
+                        return gamepad1.left_trigger>0.75;
+                    }
+                };
+        this.clawReleasePosition.pressedHandler =
+                new Handler()
+                {
+                    @Override
+                    public void invoke()
+                    {
+                        claw.setReleasePosition();
+                    }
+                };
+
+        this.clawRestPosition = new Button();
+        this.clawRestPosition.isPressed =
+                new Func<Boolean>()
+                {
+                    @Override
+                    public Boolean value()
+                    {
+                        return gamepad1.dpad_down;
+                    }
+                };
+        this.clawRestPosition.pressedHandler =
+                new Handler()
+                {
+                    @Override
+                    public void invoke()
+                    {
+                        claw.setRestPosition();
+                    }
+                };
+
+
+        //ELEVATOR
+
+        this.elevatorTopPos = new Button();
+        this.elevatorTopPos.isPressed =
+                new Func<Boolean>()
+                {
+                    @Override
+                    public Boolean value()
+                    {
+                        return gamepad2.dpad_up;
+                    }
+                };
+        this.elevatorTopPos.pressedHandler =
+                new Handler()
+                {
+                    @Override
+                    public void invoke()
+                    {
+                        elevator.runMotorUp();
+                    }
+                };
+
+        //Goes to zero
+        this.elevatorLoadPosition1 = new Button();
+        this.elevatorLoadPosition1.isPressed =
+                new Func<Boolean>()
+                {
+                    @Override
+                    public Boolean value()
+                    {
+                        return gamepad2.a;
+                    }
+                };
+        this.elevatorLoadPosition1.pressedHandler =
+                new Handler()
+                {
+                    @Override
+                    public void invoke()
+                    {
+                        elevator.runMotorDown();
+                    }
+                };
+
+
+        this.elevatorLoadPosition2 = new Button();
+        this.elevatorLoadPosition2.isPressed =
+                new Func<Boolean>()
+                {
+                    @Override
+                    public Boolean value()
+                    {
+                        return gamepad2.dpad_down;
+                    }
+                };
+        this.elevatorLoadPosition2.pressedHandler =
+                new Handler()
+                {
+                    @Override
+                    public void invoke()
+                    {
+                        elevator.goToLoadPos2();
+                    }
+                };
+
+        this.elevatorUnloadPosition1 = new Button();
+        this.elevatorUnloadPosition1.isPressed =
+                new Func<Boolean>()
+                {
+                    @Override
+                    public Boolean value()
+                    {
+                        return gamepad2.b;
+                    }
+                };
+        this.elevatorUnloadPosition1.pressedHandler =
+                new Handler()
+                {
+                    @Override
+                    public void invoke()
+                    {
+                        elevator.goToUnloadPos1();
+                    }
+                };
+
+        this.elevatorUnloadPosition2 = new Button();
+        this.elevatorUnloadPosition2.isPressed =
+                new Func<Boolean>()
+                {
+                    @Override
+                    public Boolean value()
+                    {
+                        return gamepad2.x;
+                    }
+                };
+        this.elevatorUnloadPosition2.pressedHandler =
+                new Handler()
+                {
+                    @Override
+                    public void invoke()
+                    {
+                        elevator.goToUnloadPos2();
+                    }
+                };
+
+        this.elevatorUnloadPosition3 = new Button();
+        this.elevatorUnloadPosition3.isPressed =
+                new Func<Boolean>()
+                {
+                    @Override
+                    public Boolean value()
+                    {
+                        return gamepad2.y;
+                    }
+                };
+        this.elevatorUnloadPosition3.pressedHandler =
+                new Handler()
+                {
+                    @Override
+                    public void invoke()
+                    {
+                        elevator.goToUnloadPos3();
+                    }
+                };
+
+        this.elevatorStackPosition = new Button();
+        this.elevatorStackPosition.isPressed =
+                new Func<Boolean>()
+                {
+                    @Override
+                    public Boolean value()
+                    {
+                        return gamepad2.dpad_down;
+                    }
+                };
+        this.elevatorStackPosition.pressedHandler =
+                new Handler()
+                {
+                    @Override
+                    public void invoke()
+                    {
+                        elevator.goToStackPos();
+                    }
+                };
+
+
+
+    }
+    @Override
+    public void loop() {
+        clawRestPosition.testAndHandle();
+        clawLoadPosition.testAndHandle();
+        clawReleasePosition.testAndHandle();
+        elevator.checkForBottom(telemetry);
+        elevator.checkForTop();
+        elevatorLoadPosition1.testAndHandle();
+        elevatorLoadPosition2.testAndHandle();
+        elevatorUnloadPosition1.testAndHandle();
+        elevatorUnloadPosition2.testAndHandle();
+        elevatorUnloadPosition3.testAndHandle();
+        this.driveSystem.mecanumDrive(gamepad1.right_stick_x, gamepad1.right_stick_y, gamepad1.left_stick_x, gamepad1.left_stick_y);
+
+    }
+
+
+    @Override
+    public void stop() {
+
+    }
+
+
+}
