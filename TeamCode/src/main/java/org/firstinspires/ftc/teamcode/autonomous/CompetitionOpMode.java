ed2f3c83987f84a198bb9814e2f8e1f3c9249445
StevenAbbott <sbabbott08@gmail.com>
2017-11-05 10:37:39 -0800

    improves implementation of pixy code into competition opmode and changes cryptobox dead reckoning to make use of turn and driveToPositionInches rather than driveToPositionInches and DriveXY

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CompetitionOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CompetitionOpMode.java
@@ -12,131 +12,139 @@ import org.firstinspires.ftc.teamcode.hardware.pixycam.PixyCam;
 
 @Autonomous(name="CompetitionOpMode", group="Bot")
 public class CompetitionOpMode extends AutonomousOpMode {
-
-    /*
-    private PixyCam pixyCam;
-    */
+    private final int zone = 0;
 
 
-    //private PixyCam pixyCam2;
+    //////////// PIXY DECLARATIONS/////////
+    /*private PixyCam pixyCam;
+    private PixyCam pixyCam2;
 
+    private double rightHorizServoCenter;
+    private double rightVertServoTop;
+    private double rightVertServoBottom;
+    private double leftHorizServoCenter;
+    private double leftVertServoTop;
+    private double leftVertServoBottom;
 
-    /*
-    private double rightServoCenter;
     private PixyCam.Block leftRedBlock;
     private PixyCam.Block leftBlueBlock;
-    */
+    private PixyCam.Block rightRedBlock;
+    private PixyCam.Block rightBlueBlock;
 
-
-    //private PixyCam.Block rightRedBlock;
-    //private PixyCam.Block rightBlueBlock;
-
-    //Servo rightVertServo;
+    Servo rightVertServo;
     Servo leftVertServo;
-    //Servo rightHorizServo;
-    Servo leftHorizServo;
-
-
-    /*
+    Servo rightHorizServo;
+    Servo leftHorizServo;*/
     boolean teamColorIsBlue;
-    */
-
+    //////////////////////////////////////////////////////
 
     @Override
     public void runOpMode() {
         initializeAllDevices();
         claw.setReleasePosition();
         elevator.goToZero(telemetry);
+        //initPixy();
         waitForStart();
-        /*pixyCam = hardwareMap.get(PixyCam.class, "pixycam");
-        //pixyCam2 = hardwareMap.get(PixyCam.class, "pixycam2");
+        teamColorIsBlue = (zone == 0 || zone == 1);
 
-        this.teamColorIsBlue = true;
 
-        //this.rightVertServo = hardwareMap.servo.get("rightvertservo");
-        this.leftVertServo = hardwareMap.servo.get("leftvertservo");
-        //this.rightHorizServo = hardwareMap.servo.get("righthorizservo");
-        this.leftHorizServo = hardwareMap.servo.get("lefthorizservo");
-        this.rightServoCenter = 0.4;
-        waitForStart();
+        //doPixy();
 
-        if (teamColorIsBlue == true) {
-            this.leftRedBlock = pixyCam.GetBiggestBlock(1);
-            this.leftBlueBlock = pixyCam.GetBiggestBlock(2);
-            telemetry.addData("Red:", leftRedBlock.toString());
-            telemetry.addData("Blue:", leftBlueBlock.toString());
-            telemetry.update();
-            sleep(1000);
-            leftVertServo.setPosition(0.8);
-            sleep(1000);
-            if (leftRedBlock.x < leftBlueBlock.x) {  // if red is further left than blue
-                leftHorizServo.setPosition(0.7); // then move the servo left
-                sleep(1000);
-                leftHorizServo.setPosition(0.5);
-            } else {
-                leftHorizServo.setPosition(0.3);
-                sleep(1000);
-                leftHorizServo.setPosition(0.5); // move the servo right -- also, if no values are found for x, it will go right
-            }
-            sleep(1000);
-            leftVertServo.setPosition(0);
-            sleep(1000);
-        } else { /*
-            //this.rightRedBlock = pixyCam2.GetBiggestBlock(1);
-            //this.rightBlueBlock = pixyCam2.GetBiggestBlock(2);
-            //telemetry.addData("Red:", rightRedBlock.toString());
-            //telemetry.addData("Blue:", rightBlueBlock.toString());
-            //telemetry.update();
-            sleep(1000);
-            rightVertServo.setPosition(0);
-            sleep(1000);
-            if (leftRedBlock.x < leftBlueBlock.x 1 < 2) {  // if red is further left than blue
-                rightHorizServo.setPosition(0.7); // then move the servo left
-                sleep(1000);
-                rightHorizServo.setPosition(rightServoCenter);
-            } else {
-                rightHorizServo.setPosition(0.3);
-                sleep(1000);
-                rightHorizServo.setPosition(rightServoCenter); // move the servo right -- also, if no values are found for x, it will go right
-            }
-            sleep(1000);
-            rightVertServo.setPosition(0.8);
-            sleep(1000);
-        }*/
+
         cryptoBox(0);
         stop();
     }
 
     public void cryptoBox(int zone) {
         claw.setLoadPosition();
+        sleep(2000);
         elevator.goToUnloadPos1();
         // pic 0 is left     pic 1 is right      pic 2 is center
         // zone 0 is blue non-audience     zone 1 is blue audience    zone 2 is red non-audience    zone 3 is red audience
         if (zone == 0) {
             driveToPositionInches(-33, 1);
-            telemetry.addLine("doi");
-            telemetry.update();
-            sleep(2000);
-            driveSystem.mecanumDriveXY(0, -1);
-            sleep(2000);
-            driveSystem.mecanumDriveXY(0, 0);
-            telemetry.addLine("doiiiiiii");
-            telemetry.update();
-            sleep(2000);
-            driveSystem.mecanumDriveXY(0, -1);
-            sleep(2000);
-            driveSystem.mecanumDriveXY(0, 0);
+            turn(90, 1);
+            driveToPositionInches(-10, 1);
+            turn(-90, 1);
+            elevator.goToZero(telemetry);
+            claw.setReleasePosition();
+            sleep(1000);
+            driveToPositionInches(-5, 1);
+
+
         } else if (zone == 1) {
 
-        }  else if (zone == 2) {
+        } else if (zone == 2) {
             driveToPositionInches(-33, 1);
-            driveSystem.mecanumDriveXY(0, -1);
-            sleep(2000);
-            driveSystem.mecanumDriveXY(0, 0);
+            turn(-90, 1);
+            driveToPositionInches(-10, 1);
+            turn(90, 1);
+            elevator.goToZero(telemetry);
+            claw.setReleasePosition();
+            sleep(1000);
+            driveToPositionInches(-5, 1);
         } else {
 
         }
         claw.setReleasePosition();
     }
+
+    /*public void initPixy() {
+        this.pixyCam = hardwareMap.get(PixyCam.class, "pixycam");
+        this.pixyCam2 = hardwareMap.get(PixyCam.class, "pixycam2");
+        //this.rightVertServo = hardwareMap.servo.get("rightvertservo");
+        this.leftVertServo = hardwareMap.servo.get("leftvertservo");
+        //this.rightHorizServo = hardwareMap.servo.get("righthorizservo");
+        this.leftHorizServo = hardwareMap.servo.get("lefthorizservo");
+        //this.rightHorizServoCenter = 0.4;
+        //this.rightVertServoTop = 1.0;
+        //this.rightVertServoBottom = 0.0;
+    }
+
+    public void doPixy() {
+        if (teamColorIsBlue == true) {
+            this.leftRedBlock = pixyCam.GetBiggestBlock(1);
+            this.leftBlueBlock = pixyCam.GetBiggestBlock(2);
+            telemetry.addData("Red :", leftRedBlock.toString());
+            telemetry.addData("Blue :", leftBlueBlock.toString());
+            telemetry.update();
+            sleep(1000);
+            leftVertServo.setPosition(1);
+            sleep(1000);
+            if (leftRedBlock.x < leftBlueBlock.x) {  // if red is further left than blue
+                leftHorizServo.setPosition(0.8); // then move the servo left
+                sleep(1000);
+                leftHorizServo.setPosition(0.5);
+            } else {
+                leftHorizServo.setPosition(0.2);
+                sleep(1000);
+                leftHorizServo.setPosition(0.5); // move the servo right -- also, if no values are found for x, it will go right
+            }
+            sleep(1000);
+            leftVertServo.setPosition(0);
+            sleep(1000);
+        } /*else {
+            this.rightRedBlock = pixyCam2.GetBiggestBlock(1);
+            this.rightBlueBlock = pixyCam2.GetBiggestBlock(2);
+            telemetry.addData("Red :", rightRedBlock.toString());
+            telemetry.addData("Blue :", rightBlueBlock.toString());
+            telemetry.update();
+            sleep(1000);
+            rightVertServo.setPosition(rightVertServoBottom);
+            sleep(1000);
+            if (rightRedBlock.x < rightBlueBlock.x) {
+                rightHorizServo.setPosition(rightHorizServoCenter + 0.2);
+                sleep(1000);
+                rightHorizServo.setPosition(rightHorizServoCenter);
+            } else {
+                rightHorizServo.setPosition(rightHorizServoCenter - 0.2);
+                sleep(1000);
+                rightHorizServo.setPosition(rightHorizServoCenter);
+            }
+            sleep(1000);
+            rightVertServo.setPosition(rightVertServoTop);
+            sleep(1000);
+        }
+    }*/
 
 }
