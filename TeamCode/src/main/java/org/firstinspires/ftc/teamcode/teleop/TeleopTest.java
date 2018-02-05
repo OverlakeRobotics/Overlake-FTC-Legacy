5766d7fbc9ba0ce91302287e1f08c26aaafb470f
unholydeath785 <ivansonofcoul@gmail.com>
2018-02-05 15:19:04 -0800

    teleop test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopTest.java
@@ -11,6 +11,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.robot.systems.IMUSystem;
 import org.firstinspires.ftc.teamcode.util.config.ConfigParser;
+import org.firstinspires.ftc.teamcode.util.logger.LoggingService;
 
 @TeleOp(name="TeleOpTest", group="TeleOp")
 public class TeleopTest extends BaseOpMode {
@@ -21,12 +22,24 @@ public class TeleopTest extends BaseOpMode {
     }
 
     public void init() {
+        this.initBaseSystems();
         this.imu = new IMUSystem(this);
+        logger.setLoggingServices(LoggingService.FILE, LoggingService.TELEMETRY);
     }
 
     public void loop() {
-        logger.log(this.imu.getAcceleration().toString());
-        logger.log(this.imu.getVelocity().toString());
+        if (this.imu.getAcceleration() != null && this.imu.getVelocity() != null) {
+            logger.log(this.imu.getAcceleration().toString());
+            logger.log(this.imu.getVelocity().toString());
+        }
+
+        float rx = controller1.gamepad.right_stick_x;
+        float ry = controller1.gamepad.right_stick_y;
+        float lx = controller1.gamepad.left_stick_x;
+        float ly = controller1.gamepad.left_stick_y;
+
+        this.driveSystem.driveGodMode(rx, ry, lx, ly, 0.5f);
+        telemetry.update();
     }
 
     @Override
