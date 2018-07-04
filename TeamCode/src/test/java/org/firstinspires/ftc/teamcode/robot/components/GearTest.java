d91f136bfc6cf7f51b2bb76d2606495cd6826228
ecoulson <evan.m.coulson@gmail.com>
2018-07-03 17:34:06 -0700

    added a test file for the gear class to ensure that junit is installed and that dependencies are not corrupted like on the last attempt

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/robot/components/GearTest.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.robot.components;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GearTest {
+
+    @Test
+    public void getRatio() {
+    }
+}
